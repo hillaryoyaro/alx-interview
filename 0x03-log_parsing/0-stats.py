2bc6cd159ee9d497e2e1cf6dad6a288c61c64740
hillaryoyaro <hillaryoyaroh@gmail.com>
2023-01-19 23:54:45 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,14 +3,13 @@
 
 import re
 import sys
-
 counter = 0
 file_size = 0
 statusC_counter = {200: 0, 301: 0, 400: 0,
                    401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
-def print_stats(dict, file_s):
+def printCodes(dict, file_s):
     """Prints the status code and the number of times they appear"""
     print("File size: {}".format(file_s))
     for key in sorted(dict.keys()):
@@ -24,17 +23,18 @@ if __name__ == "__main__":
             split_string = re.split('- |"|"| " " ', str(line))
             statusC_and_file_s = split_string[-1]
             if counter != 0 and counter % 10 == 0:
-                print_stats(statusC_counter, file_size)
+                printCodes(statusC_counter, file_size)
             counter = counter + 1
             try:
                 statusC = int(statusC_and_file_s.split()[0])
                 f_size = int(statusC_and_file_s.split()[1])
+                # print("Status Code {} size {}".format(statusC, f_size))
                 if statusC in statusC_counter:
                     statusC_counter[statusC] += 1
                 file_size = file_size + f_size
-            except Exception:
+            except:
                 pass
-        print_stats(statusC_counter, file_size)
+        printCodes(statusC_counter, file_size)
     except KeyboardInterrupt:
-        print_stats(statusC_counter, file_size)
+        printCodes(statusC_counter, file_size)
         raise
